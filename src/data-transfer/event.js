45666e2c31834e3a567f017b8b12b0241614afb4
Willem Leuverink <willem@leuver.ink>
2022-01-14 17:50:19 +0100

    remove unnessecary casting

--- src/data-transfer/event.js
+++ src/data-transfer/event.js
@@ -1,12 +1,10 @@
-import moment from 'moment';
-
 export default {
     fromTask(task) {
         return {
             id: task.id,
             description: task.description,
-            start: moment(task.starts_at).format('YYYY-MM-DD HH:MM'),
-            end: moment(task.ends_at).format('YYYY-MM-DD HH:MM'),
+            start: task.starts_at.format('YYYY-MM-DD HH:MM'),
+            end: task.ends_at.format('YYYY-MM-DD HH:MM'),
 
             draggable: !task.logged,
             resizable: !task.logged,
