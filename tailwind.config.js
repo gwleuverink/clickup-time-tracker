fa337e2518636087d778a6db00f8c954d844e93e
Willem Leuverink <willem@leuver.ink>
2022-01-26 23:59:47 +0100

    remove redundant tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,4 @@
 
 module.exports = {
-    content: ["./src/**/*.{html,js,vue}"],
-    theme: {
-        extend: {
-            animation: {
-                'spin-ccw': 'reverse-spin 1s linear infinite'
-            },
-            keyframes: {
-                'reverse-spin': {
-                    from: {
-                        transform: 'rotate(360deg)'
-                    },
-                }
-            }
-        },
-    },
-    plugins: [],
+    content: ["./src/**/*.{html,js,vue}"]
 }
