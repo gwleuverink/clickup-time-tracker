c30172ad9b6d5526524ac92e555dce88de819f97
Willem Leuverink <willem@leuver.ink>
2022-01-26 10:17:29 +0100

    apply theme override

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
+  <n-configProvider :theme-overrides="theme">
   <n-notification-provider>
+
     <TimeTracker />
+
   </n-notification-provider>
+  </n-configProvider>
 </template>
 
 <script>
 import TimeTracker from './components/TimeTracker.vue'
-import { NNotificationProvider } from 'naive-ui'
+import { NConfigProvider, NNotificationProvider } from 'naive-ui'
+
+/**
+ * Use this for type hints under js file
+ * @type import('naive-ui').GlobalThemeOverrides
+ */
+const theme = {
+    common: {
+        primaryColor: '#0284C7',
+        primaryColorHover: '#38BDF8',
+    }
+}
 
 export default {
   name: 'App',
   components: {
     TimeTracker,
+    NConfigProvider,
     NNotificationProvider
+  },
+  setup() {
+    return {
+      theme
+    }
   }
 }
 </script>
